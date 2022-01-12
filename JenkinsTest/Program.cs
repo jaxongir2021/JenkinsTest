89d3aa9101f55dcd27ff05d6359eefa406259b5b
jaxongir2021 <80850260+jaxongir2021@users.noreply.github.com>
2022-01-12 19:42:02 +0300

    23

--- JenkinsTest/Program.cs
+++ JenkinsTest/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace JenkinsTest
     {
         static void Main(string[] args)
         {
-            var animal = new Animal()
+            var animal = new Animal();
             Console.WriteLine(animal.GetNumberOfLegs(4));
         }
     }
