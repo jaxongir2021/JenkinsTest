c1bb1ef75ea11f7d3db37412a4304a42182dab47
jaxongir2021 <80850260+jaxongir2021@users.noreply.github.com>
2022-01-12 19:32:54 +0300

    123

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ MD C:\Results\JenkinsMsTest
 cd C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin
 dotnet restore C:\Repos\JenkinsTest
 msbuild.exe C:\Repos\JenkinsTest\JenkinsTest /restore /t:Build /p:OutputPath="C:\Results\JenkinsTest"
-msbuild.exe C:\Repos\JenkinsTest\JenkinsMsTest /restore /t:Build /p:OutputPath="C:\Results\JenkinsMsTest";
+msbuild.exe C:\Repos\JenkinsTest\JenkinsMsTest /restore /t:Build /p:OutputPath="C:\Results\JenkinsMsTest"
